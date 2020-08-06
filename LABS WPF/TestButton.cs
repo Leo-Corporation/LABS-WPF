2192df12ed7e11b8fa39838beb656787e38a1da5
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-06 16:21:59 +0200

    Contrôle personnalisé

---
+++ LABS WPF/TestButton.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LABS_WPF
+{
+    /// <summary>
+    /// Suivez les étapes 1a ou 1b puis 2 pour utiliser ce contrôle personnalisé dans un fichier XAML.
+    ///
+    /// Étape 1a) Utilisation de ce contrôle personnalisé dans un fichier XAML qui existe dans le projet actif.
+    /// Ajoutez cet attribut XmlNamespace à l'élément racine du fichier de balisage où il doit 
+    /// être utilisé :
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:LABS_WPF"
+    ///
+    ///
+    /// Étape 1b) Utilisation de ce contrôle personnalisé dans un fichier XAML qui existe dans un autre projet.
+    /// Ajoutez cet attribut XmlNamespace à l'élément racine du fichier de balisage où il doit 
+    /// être utilisé :
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:LABS_WPF;assembly=LABS_WPF"
+    ///
+    /// Vous devrez également ajouter une référence du projet contenant le fichier XAML
+    /// à ce projet et regénérer pour éviter des erreurs de compilation :
+    ///
+    ///     Cliquez avec le bouton droit sur le projet cible dans l'Explorateur de solutions, puis sur
+    ///     "Ajouter une référence"->"Projets"->[Recherchez et sélectionnez ce projet]
+    ///
+    ///
+    /// Étape 2)
+    /// Utilisez à présent votre contrôle dans le fichier XAML.
+    ///
+    ///     <MyNamespace:TestButton/>
+    ///
+    /// </summary>
+    public class TestButton : Button
+    {
+        static TestButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TestButton), new FrameworkPropertyMetadata(typeof(TestButton)));
+        }
+
+        public int Radius
+        {
+            get { return (int)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value);}
+        }
+
+        // Using a DependencyProperty as the backing store for Radius.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RadiusProperty =
+            DependencyProperty.RegisterAttached("Radius", typeof(int), typeof(TestButton), new PropertyMetadata(10));
+
+        public static int GetRadius(UIElement element)
+        {
+            return (int)element.GetValue(RadiusProperty);
+        }
+
+        public static void SetRadius(UIElement element, int value)
+        {
+            element.SetValue(RadiusProperty, value);
+        }
+
+       
+    }
+}
