ce76a4bdb6190702adbeccd88296af1f178d16f2
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-06 17:22:54 +0200

    Create Definitions.cs

---
+++ LABS WPF/Classes/Definitions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABS_WPF.Classes
+{
+    /// <summary>
+    /// Définitions.
+    /// </summary>
+    public static class Definitions
+    {
+        /// <summary>
+        /// Version de l'application.
+        /// </summary>
+        public static readonly string Version = "1.0.0.2008";
+
+        /// <summary>
+        /// URL du dépôt GitHub.
+        /// </summary>
+        public static readonly string GitHubRepoUrl = "https://github.com";
+    }
+}
