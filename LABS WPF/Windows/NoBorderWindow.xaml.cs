9485e3f659bea37886bfc9266f9dd8311c0bc10e
Léo Peyronnet <leo.peyronnet@outlook.fr>
2020-08-17 15:53:57 +0200

    Fenêtre "NoBorderWindow"

---
+++ LABS WPF/Windows/NoBorderWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LABS_WPF.Windows
+{
+    /// <summary>
+    /// Logique d'interaction pour NoBorderWindow.xaml
+    /// </summary>
+    public partial class NoBorderWindow : Window
+    {
+        public NoBorderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void maximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            RefreshMaximizeRestoreButton();
+        }
+
+        private void restoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            RefreshMaximizeRestoreButton();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RefreshMaximizeRestoreButton()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.maximizeButton.Visibility = Visibility.Collapsed;
+                this.restoreButton.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                this.maximizeButton.Visibility = Visibility.Visible;
+                this.restoreButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void window_StateChanged(object sender, EventArgs e)
+        {
+            RefreshMaximizeRestoreButton();
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RefreshMaximizeRestoreButton();
+        }
+    }
+}
